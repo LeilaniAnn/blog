ef01dae6b0a5be5c1412b6f9bf3ce5f5d0f9d269
Leilani Ann Raranga <retrosole@gmail.com>
2016-08-05 00:12:46 -0700

    Change: if user not logged on, they're redirected to login page and unable to post

--- main.py
+++ main.py
@@ -173,7 +173,7 @@ class NewPost(BlogHandler):
 
     def post(self):
         if not self.user:
-            self.redirect('/blog')
+            self.redirect('/login')
 
         subject = self.request.get('subject')
         content = self.request.get('content')
