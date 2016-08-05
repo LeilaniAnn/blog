eb148c166174179e6d50d7148164e9d226825b3d
Leilani Ann Raranga <retrosole@gmail.com>
2016-08-05 00:11:09 -0700

    Feature : users cannot like their own posts
    Users are not able to like their own posts and formatted  code to Pep8
    standard

--- main.py
+++ main.py
@@ -10,24 +10,29 @@ import jinja2
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-                               autoescape = True)
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+                               autoescape=True)
 
 secret = 'secretkeyissecret'
+
 
 def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
 
+
 def make_secure_val(val):
     return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
+
 
 def check_secure_val(secure_val):
     val = secure_val.split('|')[0]
     if secure_val == make_secure_val(val):
         return val
 
+
 class BlogHandler(webapp2.RequestHandler):
+
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
@@ -59,40 +64,46 @@ class BlogHandler(webapp2.RequestHandler):
         uid = self.read_secure_cookie('user_id')
         self.user = uid and User.by_id(int(uid))
 
+
 def render_post(response, post):
     response.out.write('<b>' + post.subject + '</b><br>')
     response.out.write(post.content)
 
+
 class MainPage(BlogHandler):
-  def get(self):
-      self.write('Hello, Udacity!')
+
+    def get(self):
+        self.write('Hello, Udacity!')
 
 
-##### user stuff
-def make_salt(length = 5):
+def make_salt(length=5):
     return ''.join(random.choice(letters) for x in xrange(length))
 
-def make_pw_hash(name, pw, salt = None):
+
+def make_pw_hash(name, pw, salt=None):
     if not salt:
         salt = make_salt()
     h = hashlib.sha256(name + pw + salt).hexdigest()
     return '%s,%s' % (salt, h)
 
+
 def valid_pw(name, password, h):
     salt = h.split(',')[0]
     return h == make_pw_hash(name, password, salt)
 
-def users_key(group = 'default'):
+
+def users_key(group='default'):
     return db.Key.from_path('users', group)
 
+
 class User(db.Model):
-    name = db.StringProperty(required = True)
-    pw_hash = db.StringProperty(required = True)
+    name = db.StringProperty(required=True)
+    pw_hash = db.StringProperty(required=True)
     email = db.StringProperty()
 
     @classmethod
     def by_id(cls, uid):
-        return User.get_by_id(uid, parent = users_key())
+        return User.get_by_id(uid, parent=users_key())
 
     @classmethod
     def by_name(cls, name):
@@ -100,12 +111,12 @@ class User(db.Model):
         return u
 
     @classmethod
-    def register(cls, name, pw, email = None):
+    def register(cls, name, pw, email=None):
         pw_hash = make_pw_hash(name, pw)
-        return User(parent = users_key(),
-                    name = name,
-                    pw_hash = pw_hash,
-                    email = email)
+        return User(parent=users_key(),
+                    name=name,
+                    pw_hash=pw_hash,
+                    email=email)
 
     @classmethod
     def login(cls, name, pw):
@@ -114,29 +125,33 @@ class User(db.Model):
             return u
 
 
-##### blog stuff
 
-def blog_key(name = 'default'):
+def blog_key(name='default'):
     return db.Key.from_path('blogs', name)
 
+
 class Post(db.Model):
-    subject = db.StringProperty(required = True)
-    content = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
     author = db.StringProperty()
     likes = db.IntegerProperty()
     liked_by = db.ListProperty(str)
 
     def render(self):
         self._render_text = self.content.replace('\n', '<br>')
-        return render_str("post.html", p = self)
+        return render_str("post.html", p=self)
+
 
 class BlogFront(BlogHandler):
+
     def get(self):
         posts = greetings = Post.all().order('-created')
-        self.render('front.html', posts = posts)
-# Blog Posts ===========================================================
+        self.render('front.html', posts=posts)
+
+
 class PostPage(BlogHandler):
+
     def get(self, post_id):
         key = db.Key.from_path('Post', int(post_id), parent=blog_key())
         post = db.get(key)
@@ -145,10 +160,11 @@ class PostPage(BlogHandler):
             self.error(404)
             return
 
-        self.render("permalink.html", post = post)
+        self.render("permalink.html", post=post)
 
 
 class NewPost(BlogHandler):
+
     def get(self):
         if self.user:
             self.render("newpost.html")
@@ -163,13 +179,18 @@ class NewPost(BlogHandler):
         content = self.request.get('content')
         author = self.request.get('author')
         if subject and content:
-            p = Post(parent=blog_key(), subject=subject, content=content, author=author, likes=0, liked_by=[])
+            p = Post(parent=blog_key(), subject=subject,
+                     content=content, author=author, likes=0, liked_by=[])
             p.put()
             self.redirect('/blog/%s' % str(p.key().id()))
         else:
             error = "subject and content, please!"
-            self.render("newpost.html", subject=subject, content=content, error=error)
+            self.render("newpost.html", subject=subject,
+                        content=content, error=error)
+
+
 class Delete(BlogHandler):
+
     def get(self, post_id):
         if not self.user:
             self.redirect('/login')
@@ -189,6 +210,7 @@ class Delete(BlogHandler):
 
 
 class LikePost(BlogHandler):
+
     def get(self, post_id):
         if not self.user:
             self.redirect('/login')
@@ -198,8 +220,8 @@ class LikePost(BlogHandler):
             author = post.author
             logged_user = self.user.name
 
-            if author == logged_user in post.liked_by: 
-                error="You can't like your own post"
+            if author == logged_user:
+                error = "You can't like your own post"
                 self.render('error.html', error=error)
 
             elif logged_user in post.liked_by:
@@ -210,12 +232,16 @@ class LikePost(BlogHandler):
                 post.liked_by.append(logged_user)
                 post.put()
                 self.redirect("/blog")
+
+
 class Error(BlogHandler):
-	def get(self):
-		self.render('error.html', error=error)
+
+    def get(self):
+        self.render('error.html', error=error)
 
 
 class EditPost(BlogHandler):
+
     def get(self, post_id):
         if not self.user:
             self.redirect('/login')
@@ -232,9 +258,8 @@ class EditPost(BlogHandler):
                 self.render("edit.html", subject=post.subject,
                             content=post.content, error=error)
             else:
-                error="you can't edit this"
+                error = "you can't edit this"
                 self.redirect("/error", error=error)
-
 
     def post(self, post_id):
         if not self.user:
@@ -248,18 +273,26 @@ class EditPost(BlogHandler):
             self.redirect('/blog/%s' % str(p.key().id()))
 
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+
+
 def valid_username(username):
     return username and USER_RE.match(username)
 
 PASS_RE = re.compile(r"^.{3,20}$")
+
+
 def valid_password(password):
     return password and PASS_RE.match(password)
 
-EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+
+
 def valid_email(email):
     return not email or EMAIL_RE.match(email)
 
+
 class Signup(BlogHandler):
+
     def get(self):
         self.render("signup-form.html")
 
@@ -270,8 +303,8 @@ class Signup(BlogHandler):
         self.verify = self.request.get('verify')
         self.email = self.request.get('email')
 
-        params = dict(username = self.username,
-                      email = self.email)
+        params = dict(username=self.username,
+                      email=self.email)
 
         if not valid_username(self.username):
             params['error_username'] = "That's not a valid username."
@@ -296,17 +329,20 @@ class Signup(BlogHandler):
     def done(self, *a, **kw):
         raise NotImplementedError
 
+
 class Unit2Signup(Signup):
+
     def done(self):
         self.redirect('/unit2/welcome?username=' + self.username)
 
+
 class Register(Signup):
+
     def done(self):
-        #make sure the user doesn't already exist
         u = User.by_name(self.username)
         if u:
             msg = 'That user already exists.'
-            self.render('signup-form.html', error_username = msg)
+            self.render('signup-form.html', error_username=msg)
         else:
             u = User.register(self.username, self.password, self.email)
             u.put()
@@ -314,7 +350,9 @@ class Register(Signup):
             self.login(u)
             self.redirect('/blog')
 
+
 class Login(BlogHandler):
+
     def get(self):
         self.render('login-form.html')
 
@@ -328,25 +366,31 @@ class Login(BlogHandler):
             self.redirect('/blog')
         else:
             msg = 'Invalid login'
-            self.render('login-form.html', error = msg)
+            self.render('login-form.html', error=msg)
+
 
 class Logout(BlogHandler):
+
     def get(self):
         self.logout()
         self.redirect('/blog')
 
+
 class Unit3Welcome(BlogHandler):
+
     def get(self):
         if self.user:
-            self.render('welcome.html', username = self.user.name)
+            self.render('welcome.html', username=self.user.name)
         else:
             self.redirect('/signup')
 
+
 class Welcome(BlogHandler):
+
     def get(self):
         username = self.request.get('username')
         if valid_username(username):
-            self.render('welcome.html', username = username)
+            self.render('welcome.html', username=username)
         else:
             self.redirect('/unit2/signup')
 
@@ -355,7 +399,7 @@ app = webapp2.WSGIApplication([('/', BlogFront),
                                ('/unit2/welcome', Welcome),
                                ('/blog/?', BlogFront),
                                ('/blog/([0-9]+)', PostPage),
-                              ('/blog/([0-9]+)/edit', EditPost),
+                               ('/blog/([0-9]+)/edit', EditPost),
                                ('/blog/([0-9]+)/like', LikePost),
                                ('/blog/([0-9]+)/delete', Delete),
                                ('/error', Error),
